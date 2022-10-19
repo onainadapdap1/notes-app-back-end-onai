9e66d26f8d4d29a74cf120f6f6a08916fa7f4efe
Onai Nadapdap <onainadapdap1@gmail.com>
2022-10-19 21:41:17 +0700

    Membuat fungsi notesService - membuat fungsi addNote

--- src/services/postgres/NotesServices.js
+++ src/services/postgres/NotesServices.js
@@ -1,9 +1,31 @@
 /*
     *FUNGSI : Mengelola resources dengan database postgreSql
-*/ 
-const {Pool} = require('pg');
+*/
+// melakukan koneksi ke database
+const { nanoid } = require('nanoid');
+const { Pool } = require('pg');
+const InvariantError = require('../../exceptions/InvariantError');
 class NotesService {
     constructor() {
-        this._pool = new Pool();
+        this._pool = new Pool(); //set instance class Pool()
     }
+    // insert data ke database
+    async addNote({ title, body, tags }) {
+        const id = nanoid(16);
+        const createdAt = new Date().toISOString();
+        const updatedAt = createdAt;
+        // objek queri untuk memasukkan note ke dalam db
+        const query = {
+            text: 'INSERT INTO notes VALUES($1, $2, $3, $4, $5, $6) RETURNING id',
+            values: [id, title, body, tags, createdAt, updatedAt],
+        }
+        // mengekseskusi query yang dibuat 
+        const result = await this._pool.query(query);
+        // cek apakah note berhasil ditambahkan ke dalam database
+        if(!result.rows[0].id) {
+            throw new InvariantError('Catatan gagal ditambahkan');
+        }
+        return result.rows[0].id;
+    }
+
 }
