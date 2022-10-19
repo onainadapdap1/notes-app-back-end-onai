85dca2f62ba1c3691db3bce9913d82a42374e742
Onai Nadapdap <onainadapdap1@gmail.com>
2022-10-19 16:16:10 +0700

    memasang node-postgres, memasang dotenv dan improve server.js

--- src/server.js
+++ src/server.js
@@ -1,3 +1,5 @@
+// mengimport dotenv dan mengkonfigurasinya
+require('dotenv').config();
 const Hapi = require('@hapi/hapi');
 const notes = require('./api/notes'); //plugins
 const NotesService = require('./services/inMemory/NotesServices')
@@ -7,8 +9,9 @@ const init = async () => {
     // create instance NotesService
     const notesService = new NotesService();
     const server = Hapi.server({
-        port: 5000,
-        host: process.env.NODE_ENV !== 'production' ? 'localhost' : '0.0.0.0',
+        // nilai dari variable environment yang ada di berkas .env dapat diakses melalui properti process.env.
+        port: process.env.PORT,
+        host: process.env.HOST,
         routes: {
             cors: {
                 origin: ['*'],
